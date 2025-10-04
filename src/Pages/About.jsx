7a0a10409c9051faf4d2bc032d19235d5f9a8299
GoTouchGra55 <shauryatamang2009@gmail.com>
2025-10-04 09:52:29 +0545

    Page setup + Routing

---
+++ src/Pages/About.jsx
@@ -0,0 +1,26 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+import Earth from "../Components/Earth";
+import { Suspense } from "react";
+import Loader from "../Components/Loader";
+
+const About = () => {
+  return (
+    <div className="relative w-screen h-screen">
+      <Canvas className="bg-black">
+        <ambientLight intensity={0.2} />
+        <pointLight intensity={2} decay={0.1} position={[10, 5, 9]}/>
+        <Stars count={400} depth={200} />
+        <OrbitControls />
+        <Suspense fallback={<Loader />}>
+          <Physics>
+            <Earth />
+          </Physics>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default About;
